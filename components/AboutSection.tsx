4b078cd8f0abc25b5fbbdbfbfc20b397223128b0
Akash Madhusudan <amverma48@gmail.com>
2025-10-15 14:28:55 +0200

    Update AboutSection.tsx - removed apostrophe

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -15,7 +15,7 @@ export default function AboutSection() {
         <div className="bg-white rounded-2xl p-8 md:p-12 shadow-lg">
           <p className="text-lg md:text-xl text-[#1B1F3B] leading-relaxed text-center">
             4Mica is a lightweight overlay service that enables any service to extend cryptographically-backed lines-of-credit across any 
-            blockchain; Bitcoin, Ethereum, Solana, and beyond. 4Mica is designed to fix web3&apos;s inefficient pre-funded model, acting as a credit layer for instant and inexpensive settlements, backed by guaranteed fair-exchange. 
+            blockchain; Bitcoin, Ethereum, Solana, and beyond. 4Mica is designed to fix Web3s inefficient pre-funded model, acting as a credit layer for instant and inexpensive settlements, backed by guaranteed fair-exchange. 
             4Mica makes programmable credit accessible to everyone. 
           </p>
         </div>
